0d6158353a7132726a102dfb41a6addb01110e8d
summertides <fdfgehge@gmail.com>
2023-11-13 15:11:16 -0500

    Scratching the surface #2

--- fbla/scripts/main/script.js
+++ fbla/scripts/main/script.js
@@ -1,5 +1,12 @@
 const menuToggle = document.querySelector('.menu-toggle');
+const navBg = document.querySelector('#nav-backdrop');
 
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active');
+
+    document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
 });
+
+navBg.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+})
