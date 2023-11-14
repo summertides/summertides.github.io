a4d4623351a455c41790457c398e624b19fb15cb
summertides <fdfgehge@gmail.com>
2023-11-14 14:51:44 -0500

    Picked the ice #1

--- fbla/scripts/global/script.js
+++ fbla/scripts/global/script.js
@@ -1,3 +1,7 @@
-const toggleNav = () => {
+const menuToggle = document.querySelector('.menu-toggle');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+
     document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
-}
+});
