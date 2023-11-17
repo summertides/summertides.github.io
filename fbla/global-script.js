009d78938bcb4565b4aa2a8beb5d50861e261205
summertides <fdfgehge@gmail.com>
2023-11-16 22:40:34 -0500

    Bug fixes for nav menu
    fixing the nav menu cause it was broken a little

--- fbla/global-script.js
+++ fbla/global-script.js
@@ -1,19 +1,20 @@
-const menuToggle = document.querySelector('.menu-toggle');
+window.onload = function () {
+    const selection1 = document.getElementById('nav3');
+    const backbutton = document.querySelector('.nav-back');
+    const menuToggle = document.querySelector('.menu-toggle');
+    
+    menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active');
+    
+        document.body.dataset.navigation = document.body.dataset.navigation === "true" ? "false" : "true";
+        document.body.dataset.selection = "home"
+    });
 
-menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active');
+    selection1.addEventListener('click', () => {
+        document.body.dataset.selection = "services"
+    })
 
-    document.body.dataset.navigation = document.body.dataset.navigation === "true" ? "false" : "true";
-});
-
-const backbutton = document.getElementById('nav-back');
-
-backbutton.addEventListener('click', () => {
-    document.body.dataset.selection = "home"
-})
-
-const selection1 = document.getElementById('nav3');
-
-selection1.addEventListener('click', () => {
-    document.body.dataset.selection = "services"
-});
+    backbutton.addEventListener('click', () => {
+        document.body.dataset.selection = "home"
+    });
+}
