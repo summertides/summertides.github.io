4e80731dca9c58c299c39aeaba113dc9f9d78509
summertides <fdfgehge@gmail.com>
2023-12-07 16:09:52 -0500

    Major Developments.

---
+++ scripts/global.js
@@ -0,0 +1,25 @@
+import LocomotiveScroll from "locomotive-scroll";
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
+
+var lastScrollTop = 0;
+var hiddenObject = document.querySelector('nav');
+
+scroll.on('scroll', function (args) {
+    var currentScroll = args.scroll.y;
+
+    if (currentScroll > lastScrollTop) {
+        // Scrolling down
+        hiddenObject.style.opacity = '0';
+        hiddenObject.style.top = '0vh';
+    } else {
+        // Scrolling up
+        hiddenObject.style.opacity = '1';
+        hiddenObject.style.top = '5vh';
+    }
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+});
